5511bfa26746af761e60902b2eaec8e9b15d6fe9
guish1 <guishgoo@gmail.com>
2020-09-19 15:23:23 +0200

    Change emoji ID with symbol

--- src/services/bot/commands.js
+++ src/services/bot/commands.js
@@ -5,7 +5,7 @@ const onCreateGroup = (message) => {
     const embed = createMessageEmbed('Create group');
     embed.addField('X', 'X');
     message.channel.send({ embed });
-    message.react("756827303664943166");
+    message.react('✌');
 
     message.awaitReactions((reaction, user) => (reaction.emoji.name == '✌'), 
     { max: 10, time: 0, errors: ['time'] })
@@ -13,7 +13,7 @@ const onCreateGroup = (message) => {
 		const reaction = collected.first();
 
 		if (reaction.emoji.name === '✌') {
-			message.reply('you reacted with a thumbs up.');
+			message.reply('You reacted.');
 		}
 	})
 	.catch(collected => {
