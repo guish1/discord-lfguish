a4ff1f8c459b8295d7e3473b0c55defc2a14e439
guish1 <guishgoo@gmail.com>
2020-09-19 16:23:41 +0200

    try: create role and channel

--- src/services/bot/commands.js
+++ src/services/bot/commands.js
@@ -2,14 +2,27 @@ const { createMessageEmbed } = require('../messages/embed');
 
 const onCreateGroup = (message) => {
     const embed = createMessageEmbed('Create group');
-    embed.addField('X', 'X');
+    const group_name = "group-" + Math.random();
+    embed.addField('Name', group_name);
     message.channel.send({ embed }).then(function(msg) {
         msg.react('✌');
     });
 
-    message.guild.channels.create(message.author.username, {
+    message.guild.roles.create(group_name);
+
+    message.guild.channels.create(group_name, {
         type:   'text',
-        parent: '466734004318502934'
+        parent: '466734004318502934',
+        permissionOverwrites: [
+            {
+                id:     message.guild.roles.everyone,
+                deny:   ['VIEW_CHANNEL'] 
+            },
+            {
+                id:     group_name,
+                allow:  ['VIEW_CHANNEL']
+            }
+        ]
     })
 
     message.awaitReactions((reaction, user) => (reaction.emoji.name == '✌'), 
