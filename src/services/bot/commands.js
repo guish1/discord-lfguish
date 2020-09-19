38675283da1692ded4d5954546cf70d0ec9cf702
guish1 <guishgoo@gmail.com>
2020-09-19 15:36:40 +0200

    try: create channel on command

--- src/services/bot/commands.js
+++ src/services/bot/commands.js
@@ -1,12 +1,13 @@
 const { createMessageEmbed } = require('../messages/embed');
 
-
 const onCreateGroup = (message) => {
     const embed = createMessageEmbed('Create group');
     embed.addField('X', 'X');
     message.channel.send({ embed }).then(function(msg) {
         msg.react('✌');
     });
+
+    message.guild.createChannel(message.author.username, 'text');
 
     message.awaitReactions((reaction, user) => (reaction.emoji.name == '✌'), 
     { max: 10, time: 0, errors: ['time'] })
