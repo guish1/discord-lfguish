63a1e388735e135f7d077f116ab6db992527ecc9
guish1 <guishgoo@gmail.com>
2020-09-20 17:46:36 +0200

    fix module path

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(); 
 
-const { getGames } = require('../lfguish/games');
+const { getGames } = require('../../lfguish/games');
 
 const {
     onCreateGroup,
